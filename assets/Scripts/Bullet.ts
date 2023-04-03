a8c62a3b98827bb4aeffde6bad716060dee434a0
DuyTrongk64 <duytrongk64@gmail.com>
2023-04-03 12:00:32 +0700

    final collision

--- assets/Scripts/Bullet.ts
+++ assets/Scripts/Bullet.ts
@@ -6,6 +6,15 @@ export default class NewClass extends cc.Component {
     @property()
     speed = 0;
 
+    collidingWithEdge(){
+        let worldRect = this.node.getBoundingBoxToWorld();
+        let screenSize = cc.view.getVisibleSize();
+        if (worldRect.yMin < 0 || worldRect.yMax > screenSize.height) {
+            // destroy nếu va chạm với viền màn hình
+            this.node.destroy();
+        }
+    }
+
     onCollisionEnter(other: cc.PhysicsCollider, self: cc.PhysicsCollider){
         //console.log(`Collided with ${other.node.name}!`);
         if(other.node.name == 'chicken'){
@@ -20,9 +29,9 @@ export default class NewClass extends cc.Component {
     }
 
     update(dt) {
+        this.collidingWithEdge();
         let direction = new cc.Vec3(0, 1, 0);
         let newPosition = this.node.position.add(direction.multiplyScalar(this.speed * dt));
         this.node.setPosition(newPosition);
-
     }
 }
--- assets/Scripts/Chicken.ts
+++ assets/Scripts/Chicken.ts
@@ -16,6 +16,15 @@ export default class NewClass extends cc.Component {
     @property
     speed = 0;
 
+    collidingWithEdge(){
+        let worldRect = this.node.getBoundingBoxToWorld();
+        let screenSize = cc.view.getVisibleSize();
+        if (worldRect.yMin < 0 || worldRect.yMax > screenSize.height) {
+            // destroy nếu va chạm với viền màn hình
+            this.node.destroy();
+        }
+    }
+
     onLoad () {
         //set collider
         var manager = cc.director.getCollisionManager();
@@ -41,10 +50,9 @@ export default class NewClass extends cc.Component {
     }
 
     update (dt) {
-        // let direction = new cc.Vec3(0, -1, 0);
-        // let newPosition = this.node.position.add(direction.multiplyScalar(this.speed * dt));
-        // this.node.setPosition(newPosition);
-        
-        
+        this.collidingWithEdge();
+        let direction = new cc.Vec3(0, -1, 0);
+        let newPosition = this.node.position.add(direction.multiplyScalar(this.speed * dt));
+        this.node.setPosition(newPosition);
     }
 }
